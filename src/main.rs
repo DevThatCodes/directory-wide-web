d41f009d36957d1bd88483dd90665136e66cec5a
DevThatCodes <cupcooklinux@gmail.com>
2024-09-22 22:07:36 -0400

    parse_to_complex_taglist() now returns the data inside the tags

--- src/main.rs
+++ src/main.rs
@@ -10,39 +10,58 @@ pub mod rml_parser {
     }
 
     impl RmlElement {
-        fn create_element(name: String, attributes: Vec<(String,String)>) -> Self {
+        pub fn create_element(name: String, attributes: Vec<(String,String)>) -> Self {
             RmlElement {name, attributes, children: vec![]}
         }
-        fn get_children(&self) -> &Vec<RmlElement> {
+        pub fn get_children(&self) -> &Vec<RmlElement> {
             &self.children
         }
-        fn add_child(&mut self, element: RmlElement) {
+        pub fn add_child(&mut self, element: RmlElement) {
             self.children.push(element);
         }
-        fn tag_name(&self) -> &str {
+        pub fn tag_name(&self) -> &str {
             &self.name
         }
-        fn get_attributes(&self) -> &Vec<(String, String)> {
+        pub fn get_attributes(&self) -> &Vec<(String, String)> {
             &self.attributes
         }
     }
 
     pub fn parse_to_list(string: &str) -> Vec<String> {
         let mut tag_builder : String = String::from("");
+        let mut tag_content_builder : String = String::from("");
         let mut tag_stack : Vec<String> = vec![];
         let mut inside_tag = false;
+        let mut after_tag = false;
         for c in string.chars() {
+            // do data between tags
+            if after_tag {
+                if c != '[' {
+                    tag_content_builder = tag_content_builder + &c.to_string();
+                }
+                //do some other stuff that is after a tag but doesnt depend on ^
+            }
+
+            // do inside tag data
             if inside_tag {
                 if c == ']' {
                     inside_tag = false;
+                    after_tag = true;
                     tag_stack.push(tag_builder);
                     tag_builder = String::from("");
                 } else {
                     tag_builder = tag_builder + &c.to_string();
                 }
             }
+
             // check if inside tag
             if c == '[' {
+                if after_tag && !tag_content_builder.replace("\n", "").replace(" ", "").is_empty() {
+                    tag_content_builder = String::from("~") + &tag_content_builder;
+                    tag_stack.push(tag_content_builder);
+                }
+                tag_content_builder = String::from("");
+                after_tag = false;
                 inside_tag = true;
             }
         }
@@ -52,18 +71,22 @@ pub mod rml_parser {
     pub fn parse_to_complex_taglist(taglist: Vec<String>) -> Vec<(String, Vec<String>)> {
         let mut complex_taglist : Vec<(String, Vec<String>)> = vec![];
         for tag in taglist {
-            let tag_data_unchanged : Vec<&str> = tag.split_whitespace().collect();
-            let mut tag_data : Vec<String> = vec![];
-            for string in tag_data_unchanged {
-                tag_data.push(string.to_string());
+            if tag[0..1] == *"~" {
+                complex_taglist.push((tag[1..tag.len()].to_string(), vec![String::from("not-tag")]));
+            } else {
+                let tag_data_unchanged : Vec<&str> = tag.split_whitespace().collect();
+                let mut tag_data : Vec<String> = vec![];
+                for string in tag_data_unchanged {
+                    tag_data.push(string.to_string());
+                }
+                let mut tag_name = tag_data[0].clone();
+                tag_data.remove(0);
+                if tag_name[0..1] == *":" {
+                    tag_data.push(String::from("end-tag"));
+                    tag_name.remove(0);
+                }
+                complex_taglist.push((tag_name.to_string(), tag_data));
             }
-            let mut tag_name = tag_data[0].clone();
-            tag_data.remove(0);
-            if tag_name[0..1] == *":" {
-                tag_data.push(String::from("end-tag"));
-                tag_name.remove(0);
-            }
-            complex_taglist.push((tag_name.to_string(), tag_data));
         }
         complex_taglist
     }
@@ -76,5 +99,5 @@ pub mod rml_parser {
 fn main() {
     let content = fs::read_to_string("main.info").expect("No file found");
     let doc = rml_parser::parse(rml_parser::parse_to_complex_taglist(rml_parser::parse_to_list(&content)));
-    println!("{:#?}", doc);
+    println!("doc: {:#?}\ncomplex_taglist:{:#?}", doc, rml_parser::parse_to_complex_taglist(rml_parser::parse_to_list(&content)));
 }
